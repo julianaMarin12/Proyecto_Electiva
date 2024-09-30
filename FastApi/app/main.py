f2de9def1e9ac5ef6f822a3de6ae326e70d087bb
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-09-29 20:18:33 -0500

    Create main.py

---
+++ FastApi/app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+from contextlib import asynccontextmanager
+from config.database import database as connection
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Conectar a la base de datos si la conexión está cerrada
+    if connection.is_closed():
+        connection.connect()
+    try:
+
+        yield  # Aquí es donde se ejecutará la aplicación
+    finally:
+        # Cerrar la conexión cuando la aplicación se detenga
+        if not connection.is_closed():
+            connection.close()
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.get("/")
+def read_root():
+    return RedirectResponse(url="/docs")
