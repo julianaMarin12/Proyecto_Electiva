bf896a6e844b6b8e12cb681b4a45118d98547332
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-10-06 13:52:42 -0500

    Update migrations.py

---
+++ FastApi/app/migrations.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config.settings import DATABASE
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True)
+    email = Column(String(100), unique=True, index=True)
+    hashed_password = Column(String(255))
+    idClusters = Column(Integer, foreign_keys=True, index=True)
+    photoProfile = Column(String(50))
+
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(50), index=True)
+    content = Column(String(50))
+    user_id = Column(Integer)
+
+
+# Configurar la base de datos
+DATABASE_URL = f"mysql+pymysql://{DATABASE["user"]}:{DATABASE["password"]}@{DATABASE["host"]}/{DATABASE["name"]}"
+engine = create_engine(DATABASE_URL)
+
+
+# Crear una sesión de base de datos
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
