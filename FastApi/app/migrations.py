9406d486c6c328dd4fa5a8f995b445a3fcd8e1c3
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-10-10 13:14:24 -0500

    Update migrations.py

--- FastApi/app/migrations.py
+++ FastApi/app/migrations.py
@@ -8,11 +8,10 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True)
+    idUser = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), unique=True, index=True)
     email = Column(String(100), unique=True, index=True)
-    hashed_password = Column(String(255))
-    idClusters = Column(Integer, foreign_keys=True, index=True)
+    password = Column(String(255))
     photoProfile = Column(String(50))
 
 
