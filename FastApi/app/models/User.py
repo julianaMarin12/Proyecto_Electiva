dd28c37a4a9fda41b85b51fbb5edeed407e72a7b
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-10-10 13:13:02 -0500

    Update User.py
    Documentation

--- FastApi/app/models/User.py
+++ FastApi/app/models/User.py
@@ -1,6 +1,9 @@
+"""This module defines the Pydantic  model for users"""
+
 from pydantic import BaseModel
 
 class User(BaseModel):
+    """Pydantic model for users"""
     idUsers: int
     name: str
     email: str
